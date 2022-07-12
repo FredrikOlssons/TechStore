5652f49e7c89a1c8094b60b1121059ca79d05ecb
FredrikOlssons <fredrik_olssons@live.se>
2022-07-12 15:24:40 +0200

    cart-update

--- Cart.js
+++ Cart.js
@@ -1,86 +1,311 @@
-const main =  document.getElementsByTagName("main")[0];
-const body = document.getElementsByTagName("body");
-function addCard(product){
-	const card = document.createElement("div");
-	card.className = "cardContainer";
-	const cardImage = document.createElement("img");
-	cardImage.src = product.image;
-	cardImage.className = "imageContainer";
-	const cardTitle = document.createElement("h1");
-	cardTitle.innerText = product.title;
-	cardTitle.className = "cardTitle";
-	const CardPrice = document.createElement("p");
-	CardPrice.innerText = product.price+ " " + "kr";
-	CardPrice.className = "price";
-	const CardQuantity = document.createElement('p');
-	CardQuantity.innerText = product.quantity + " " + "st";
-	CardQuantity.className = "quantity";
-	card.appendChild(cardImage);
-	card.appendChild(cardTitle);
-	card.appendChild(CardPrice);
-	card.appendChild(CardQuantity);
-	main.className = "main";
-	main.appendChild(card);
-	let btn = document.createElement("button");
-	btn.className = "btnDeletCart";
-	btn.innerHTML = `<i class="fa fa-trash-o"></i> &nbsp Ta bort`;
-	card.appendChild(btn);
-	btn.addEventListener ("click", e => {
-			deleteCard(product)
-	});
-}
-const productsStorage = localStorage.getItem("products");
-	if (productsStorage) {
-	const products = JSON.parse(productsStorage);
-	products.forEach((element) => {
-		addCard(element)
-
-	});
-	totalCostCards(products)
-	CardQuantity(products)
+function initSite() {
+	getNrOfCartItems()
+	getCartContent()
+	console.log('init körs')
 }
 
-function deleteCard(product){
-	const productsStorage = localStorage.getItem("products");
-	const products = JSON.parse(productsStorage);
-	let productIndex;
-	main.querySelectorAll(".cardContainer").forEach((card, index) => {
-	if (card.getElementsByTagName("h1")[0].innerText === product.title) {
-			productIndex = index;
+function getCartContent() {
+	let cart = localStorage.getItem('cart')
+		
+	if(cart) {
+		cart = JSON.parse(cart)
+		console.log('localStorage finns')
+	} else {
+		cart = []
 	}
-})
+	
+	let cartSection = document.getElementsByClassName('sectionCart')[0];
+	cartSection.innerHTML = '';
 
-	products.splice(productIndex, 1)
-	localStorage.setItem("products", JSON.stringify(products))
-	const deletedCard = main.querySelectorAll(".cardContainer")[productIndex]
-	main.removeChild(deletedCard)
-	totalCostCards(products)
-	CardQuantity(products)
+	for (let s = 0; s < cart.length; s++) {
+		let cartItem = createContainerForCart(cart[s]);
 
-}
-function totalCostCards(products){
-	let totalCost = 0;
-	products.forEach((product) => {
-			totalCost = totalCost + product.price * product.quantity
-		});
-	document.querySelectorAll(".total-price span")[0].innerText = totalCost;
-}
-
-function CardQuantity(products){
-	let CardQuantity = 0;
-	products.forEach((product) => {
-			CardQuantity = CardQuantity + product.quantity
-		});
-	document.querySelectorAll(".cart-icon span")[0].innerText = CardQuantity;
-}
-function popUp(){
-	if (confirm("Fortsätt till betalning.")){
-			localStorage.clear()
-			location.replace("./index.html")
-	} 
-}
-function close() {
-	if(document.getElementsByClassName('total-price').style.display=='block') {
-	  document.getElementsByClassName('total-price').style.display='none';
+		cartSection.append(cartItem) 
+		console.log('showcart')
+		
 	}
-}  
+	completePurchase(cart)
+}
+
+function createContainerForCart(cartItem) {
+	let cartItemContainer = document.createElement('div');
+	cartItemContainer.className = 'cartItem';
+			
+			let titleContainer = document.createElement('div');
+			titleContainer.classList.add('cartTitleContainer');
+			let title = document.createElement('h3');
+			title.classList.add('priceElement')
+			title.innerText = cartItem.product.title
+			titleContainer.append(title)
+
+			let cartItemImgContainer = document.createElement('div')
+			cartItemImgContainer.classList.add('cartImgContainer');
+			let cartItemImg = document.createElement('img')
+			cartItemImg.classList.add('cartImg');
+			cartItemImg.src = "./assets/" + cartItem.product.image;
+			cartItemImgContainer.append(cartItemImg);
+			
+			/* let controls = document.createElement('div');
+			controls.className = 'controls';
+			cartItem.append(controls)
+
+			let plus = document.createElement('span');
+			plus.textContent = '+';
+			plus.setAttribute('data-title', item.title);
+			controls.appendChild(plus);
+			plus.addEventListener('click', incrementCart) */
+
+			let controls = document.createElement('div');
+			controls.classList.add('quantityContainer')
+			let qty = document.createElement('span');
+			qty.classList.add('controls')
+			qty.innerText = cartItem.quantity + 'st';
+			let cost = (cartItem.quantity * cartItem.product.price + 'kr');
+			cost.innerText = cost;
+			controls.append(qty, cost);
+
+			/*
+			let minus = document.createElement('span');
+			minus.textContent = '-';
+			minus.setAttribute('data-title', item.title);
+			controls.appendChild(minus);
+			minus.addEventListener('click', decrementCart) */
+
+			let priceContainer = document.createElement('div');
+			priceContainer.classList.add('priceContainerCart');
+			let price = document.createElement('h3')
+			price.classList.add('price')
+			price.innerText = (cartItem.product.price + 'kr/st');
+			
+
+			priceContainer.append(price);
+
+
+			let removeItemButtonContainer = document.createElement('div')
+			removeItemButtonContainer.classList.add('removeBtnContainer')
+			let removeItemBtn = document.createElement('removeBtn')
+			removeItemBtn.innerText = 'Ta bort'
+			removeItemBtn.addEventListener('click', ()=>{
+				removeCartItem(cartItem)
+				getNrOfCartItems()				
+			})
+
+			removeItemButtonContainer.append(removeItemBtn);
+			cartItemContainer.append(cartItemImgContainer, titleContainer, priceContainer,  controls,  removeItemButtonContainer)
+
+			return cartItemContainer
+}
+
+
+function completePurchase(cart) {
+	
+	let totalPrice = 0
+	cart.forEach((cartItem) => {
+		totalPrice += cartItem.product.price * cartItem.quantity
+		console.log(cart)
+	});
+	
+	let checkoutAndPriceContainer = document.getElementsByClassName('priceAndCheckout')[0]
+	checkoutAndPriceContainer.innerHTML = ""
+
+	
+	let priceContainer = document.createElement('div')
+	priceContainer.classList.add('priceContainer')
+	priceContainer.innerText = 'Totalt pris:' + ' ' + totalPrice + ' kr'
+	
+	let checkoutAndPrice = document.createElement('div')
+	checkoutAndPrice.classList.add('checkoutSection')
+
+	let checkBox = document.createElement('i')
+	checkBox.className = 'fas fa-check'
+	checkBox.classList.add('checkBox')
+
+	
+	let checkout = document.createElement('div')
+	checkout.classList.add('checkoutBtn')
+	checkout.innerText = 'Slutför ditt köp'
+	checkout.addEventListener('click', () => {
+		alert('Tack för ditt köp')
+		localStorage.removeItem('cart')
+		window.location = 'index.html'
+	})
+	
+		checkoutAndPrice.append(checkBox, checkout)
+		checkoutAndPriceContainer.append(priceContainer, checkoutAndPrice)
+
+}
+
+   
+
+
+
+
+
+
+
+/* function incrementCart(ev) {
+	ev.preventDefault();
+	let title = parseInt(ev.target.getAttribute('data-id'));
+	cart.increase(title, 1); //................
+	let controls = ev.target.parentElement;
+	let qty = controls.querySelector('span:nth-child(2)');
+	let item = cart.find(title)
+	if(item){
+		qty.textContent = item.qty;
+	}else{
+		document.getElementsByClassName('cart').removeChild(controls.parentElement);
+	}
+}
+
+
+function decrementCart(ev) {
+	ev.preventDefault();
+	let title = parseInt(ev.target.getAttribute('data-id'));
+	cart.reduce(title, 1); //................
+	let controls = ev.target.parentElement;
+	let qty = controls.querySelector('span:nth-child(2)');
+	let item = cart.find(title)
+	if(item){
+		qty.textContent = item.qty;
+	}else{
+		document.getElementsByClassName('cart').removeChild(controls.parentElement);
+	}
+}
+ */
+
+function getNrOfCartItems() {
+    let saveProducts = document.getElementsByTagName("span")[0]
+	
+    let cart = localStorage.getItem("cart")
+      
+    let amount = 0
+    
+    if(!cart) {
+        saveProducts.innerText = amount
+        return
+    } 
+	
+    cart = JSON.parse(cart)
+    cart.forEach((cartItem) => {
+        amount += cartItem.quantity
+    })
+
+    saveProducts.innerText = amount 
+
+    
+}
+
+
+function removeCartItem(cartItem) {
+
+	let cart = JSON.parse(localStorage.getItem('cart'))
+	for (let i = 0; i < cart.length; i++) {
+
+		if (cartItem.product.title == cart[i].product.title) {
+			cart.splice(i, 1)
+		}
+	}
+
+		cart = JSON.stringify(cart)
+		localStorage.setItem('cart', cart)
+
+		getCartContent()		
+}
+
+
+	/* if (cartContentsFromJSON) {
+		cart.contents = JSON.parse(cartContentsFromJSON);
+	} else{
+	cart.contents = [
+		{title: 'Buns', description: 'Sweet fruit', image: 'apple', price: 10},
+		{title: 'Buts', description: 'Sweet fruit', image: 'apple', price: 100},
+		{title: 'Cheek', description: 'Sweet fruit', image: 'apple', price: 1000}
+	];
+	cart.sync()
+	}
+	},
+	async sync(){
+	let _cart = JSON.stringify(cart.contents);
+	await localStorage.setItem(cart.key, _cart)
+	},
+	find(title) {
+	let match = cart.contents.filter(item=>{
+		if(item.title == title)
+		console.log('found')
+		return true;
+	});
+	if(match && match[0])
+	return match[0];
+	},
+	add(title) {
+	if(cart.find(title)) {
+		cart.increase(title, 1);
+	}else{
+		let arr = products.filter(product=>{
+			if(product.title == title) {
+				return true
+			}
+		});
+		if(arr && arr[0]) {
+			let obj = {
+				title: arr[0].title,
+				image: arr[0].image,
+				qty: 1,
+				price: arr[0].price
+			};
+			cart.contents.push(obj);
+			console.log('added')
+			cart.sync();
+		}else{ 
+			console.error('Invalid product');
+		}
+	}
+	},
+	increase(title, qty=1){
+	cart.contents = cart.contents.map(item=>{
+		if (item.title === title) 
+		item.qty = item.qty + qty;
+		console.log('increase')
+		return item;
+	});
+	cart.sync()
+	},
+	reduce(title, qty=1){
+	cart.contents = cart.contents.map(item=>{
+		if (item.title === title)
+		item.qty = item.qty + qty;
+		console.log('reduce')
+		return item;
+	})
+	cart.contents.forEach(async, item => {
+		if(item.title === title === 0)
+		cart.remove(title);
+	});
+	cart.sync()
+	},
+	remove(title){
+	cart.contents = cart.contents.filter(item=>{
+		if(item.title !== title)
+		console.log('remove')
+		return true
+	})
+	cart.sync()
+	},
+	empty(){
+	cart.contents = [];
+	console.log('empty')
+	cart.sync()
+	},
+	/* sort(field = 'title'){
+	let sorted = cart.contents.sort((a, b)=>{
+		if(a[field] > b[field]){
+			return 1;
+		}else if(a[field] < b[field]){
+			return -1;
+		}else{
+			return 0;
+		}
+	});
+	return sorted;
+	}   
+	} */
--- main.js
+++ main.js
@@ -2,167 +2,166 @@ var listOfProducts;
 
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
-  fetch("./products.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (products) {
-      listOfProducts = products;
-      addProductsToWebpage();
-    });
+    fetch("./products.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (products) {
+            listOfProducts = products;
+            addProductsToWebpage();
+        });
 }
 
 function initSite() {
-  loadProducts();
-  // This would also be a good place to initialize other parts of the UI
+    let productNumber = localStorage.getItem("products");
+    if (productNumber) {
+        document.querySelector(".cart-icon span").textContent = productNumbers;
+    }
+    //CC()
+    loadProducts();
+    getNrOfCartItems()
+    // This would also be a good place to initialize other parts of the UI
 }
+
+
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-  // Check your console to see that the products are stored in the listOfProducts varible.
+    // Check your console to see that the products are stored in the listOfProducts varible.
 
-  let main = document.getElementsByTagName("main")[0];
+    let main = document.getElementsByTagName("main")[0];
 
-  let isGrey = true;
+    let isGrey = true;
 
-  for (let index = 0; index < listOfProducts.length; index++) {
-    const product = listOfProducts[index];
+    for (let index = 0; index < listOfProducts.length; index++) {
+        const product = listOfProducts[index];
 
-    // Skapar den yttersta produktboxen
-    let productContainer = document.createElement("div");
-    productContainer.classList.add("productContainer");
-    if (isGrey) {
-      productContainer.style.backgroundColor = "lightgray";
-    }
-
-    // Skapar box för titel
-    let titleContainer = document.createElement("div");
-    titleContainer.classList.add("titleContainer");
-    let title = document.createElement("h1");
-    title.innerText = product.title;
-
-    titleContainer.append(title);
-
-    //Skapa box för decription
-    let descriptionContainer = document.createElement("div");
-    descriptionContainer.classList.add("descriptionContainer");
-    let description = document.createElement("p");
-    description.innerText = product.description;
-
-    descriptionContainer.append(description);
-
-    // Skapa box för bild
-    let boxForImg = document.createElement("div");
-    boxForImg.classList.add("imgContainer");
-    let image = document.createElement("img");
-    image.src = "/assets/" + product.image;
-
-    boxForImg.append(image);
-
-    //Skapa box för pris
-    let priceContainer = document.createElement("div");
-    priceContainer.classList.add("priceContainer");
-    let price = document.createElement("h3");
-    price.innerText = product.price + " kr";
-
-    priceContainer.append(price);
-
-    //Skapa box för kundvagn
-    let cartContainer = document.createElement("div");
-    cartContainer.classList.add("cartContainer");
-
-    let button = document.createElement("div");
-    button.classList.add("cartButton");
-
-    let buttontext = document.createElement("h4");
-    buttontext.innerText = "Lägg till i kundvagn";
-
-    let buttonIcon = document.createElement("i");
-    buttonIcon.className = "fas fa-shopping-cart buttonIcon";
-
-    button.append(buttontext, buttonIcon);
-    button.addEventListener("click", (e) => {
-      addProductsToCart(product);
-    });
-
-    cartContainer.append(button);
-
-    productContainer.append(
-      titleContainer,
-      descriptionContainer,
-      boxForImg,
-      priceContainer,
-      cartContainer
-    );
-    main.append(productContainer);
-    isGrey = !isGrey;
-
-    function onload() {
-      let productNumber = localStorage.getItem("products");
-
-      if (productNumber) {
-        document.querySelector(".cart-icon span").textContent = productNumbers;
-      }
-    }
-
-    //Funktion för att lägga till kundvagnen
-    function addProductsToCart(product) {
-        let productsInCart = localStorage.getItem("products");
-        let products = [];
-        if (productsInCart) {
-            products = JSON.parse(productsInCart);
-            let newProductInCart = true;
-                for (let i = 0; i < products.length; i++) {
-                    const element = products[i];
-                if (element.title === product.title) {
-                    element.quantity = element.quantity +1
-                    newProductInCart = false;
-                }
-            } 
-            if (newProductInCart) {
-                products.push(product)
-                products[products.length -1].quantity = 1;
-            }
-        } else {
-            products.push(product)
-            products[0].quantity = 1;
+        // Skapar den yttersta produktboxen
+        let productContainer = document.createElement("div");
+        productContainer.classList.add("productContainer");
+        if (isGrey) {
+            productContainer.style.backgroundColor = "lightgray";
         }
 
-        localStorage.setItem("products", JSON.stringify(products))
-            let CC = 0;
-            products.forEach((product) => {
-                CC = CC + products.quantity;
-            })
+        // Skapar box för titel
+        let titleContainer = document.createElement("div");
+        titleContainer.classList.add("titleContainer");
+        let title = document.createElement("h1");
+        title.innerText = product.title;
 
-            document.querySelectorAll(".cart-icon span")[0].innerText = CC;
-        }
+        titleContainer.append(title);
 
-        function CC() {
-            let CC = 0 ; 
-            let productInCart = localStorage.getItem("products");
-        if (productInCart) {
-            let products = JSON.parse(productInCart);
-            products.forEach((product) => {
-                CC = CC + product.quantity
-            });
-        }
-        document.querySelectorAll(".cart-icon span")[0].innerText = cardQuantity;
-    }
-   }
-   onload()
-  CC()
-  }
+        //Skapa box för decription
+        let descriptionContainer = document.createElement("div");
+        descriptionContainer.classList.add("descriptionContainer");
+        let description = document.createElement("p");
+        description.innerText = product.description;
 
-      /* localStorage.setItem("ShoppingCart", JSON.stringify(productToAdd));
-      let cart = localStorage.getItem("ShoppingCart");
-      if (cart !== null && cart !== false) {
-        cart = JSON.parse(cart);
-      } else {
-        cart = [];
-      } */    
+        descriptionContainer.append(description);
 
+        // Skapa box för bild
+        let boxForImg = document.createElement("div");
+        boxForImg.classList.add("imgContainer");
+        let image = document.createElement("img");
+        image.classList.add('productImg')
+        image.src = "/assets/" + product.image;
 
+        boxForImg.append(image);
 
-  //cart.push(productToAdd)
+        //Skapa box för pris
+        let priceContainer = document.createElement("div");
+        priceContainer.classList.add("priceContainer");
+        let price = document.createElement("h3");
+        price.innerText = product.price + " kr";
+
+        priceContainer.append(price);
+
+        //Skapa box för kundvagn
+        
+
+        let buttonContainer = document.createElement("div")
+        buttonContainer.classList.add("buttonContainer")        
+        buttonContainer.addEventListener("click", () =>  {
+            addToCart(product)
+        })
+
+        let buttonAddToCart = document.createElement("button");
+        buttonAddToCart.className = "cartButton"; 
+        
+        let buttontext = document.createElement("h4");
+        buttontext.innerText = "Lägg till i kundvagn";
+        
+        let buttonIcon = document.createElement("i");
+        buttonIcon.className = "fas fa-shopping-cart buttonIcon";
+
+        buttonAddToCart.append(buttontext, buttonIcon)
+        buttonContainer.append(buttonAddToCart)
+        
+
+        productContainer.append(
+            titleContainer,
+            descriptionContainer,
+            boxForImg,
+            priceContainer,
+            buttonContainer
+        );
+        main.append(productContainer);
+        isGrey = !isGrey;
+        
+        
+}
+
+}
+
+//function onload() 
 
  
+
+function addToCart(product) {
+    
+    let cart = localStorage.getItem("cart")
+
+    if(cart) {
+        cart = JSON.parse(cart)
+    } else {
+        cart = []
+    }
+
+    let productIndex = cart.findIndex((cartItem) => {
+        return cartItem.product.title == product.title
+    })
+
+    if(productIndex >= 0) {
+        cart[productIndex].quantity++
+    } else {
+        cart.push({
+            product: product,
+            quantity: 1
+        })
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+    getNrOfCartItems()
+}
+
+function getNrOfCartItems() {
+    let saveProducts = document.getElementsByTagName("span")[0]
+
+    let cart = localStorage.getItem("cart")
+      
+    let amount = 0
+    
+    if(!cart) {
+        saveProducts.innerText = amount
+        return
+    } 
+
+    cart = JSON.parse(cart)
+    cart.forEach((cartItem) => {
+        amount += cartItem.quantity
+    })
+
+    saveProducts.innerText = amount 
+    
+}
+
